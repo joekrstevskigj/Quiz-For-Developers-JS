1b43ad522071a012306810129dd65563741495cb
JoeCross <krstevskigj@gmail.com>
2023-12-27 00:43:14 +0000

    creating the logic about displaying and guessing the correct question

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,24 +1,78 @@
 //an object for convinience to hold the references for the questions elements.
 const questionsSection = {
-    "container" : document.querySelector("#questions"),
-    "title" : document.querySelector("#question-title"),
-    "choices" : document.querySelector("#choices")
+    "container": document.querySelector("#questions"),
+    "title": document.querySelector("#question-title"),
+    "choices": document.querySelector("#choices")
+}
+const qToPlay = getQuestions();
+let correctAnswer = -1;
+
+//this resets the elements that holds the elements to empty, so it can host next 
+//question.
+const resetQuesionDisplay = (display) => {
+    display.title.textContent = "";
+
+    while (display.choices.firstChild) {
+        display.choices.removeChild(display.choices.firstChild);
+    }
 }
 
-const init = () => { 
+const createChoiceChildren = (parent, choices) => {
+    for (let i = 0; i < choices.length; i++) {
+        const choice = document.createElement("button");
+        choice.dataset.index = i.toString();
+        choice.textContent = choices[i];
+        choice.addEventListener('click', onClickChoice);
 
-    const onClickStartButton = (e) => {
-        //we don't need the event hanlder anymore, so let's remove it.
-        e.currentTarget.removeEventListener("click", onClickStartButton);
-        
-        //hide the start screen, so we can show the questions instead.
-        document.getElementById("start-screen").classList.toggle("hide");
-        
-        //show the section with the questions
-        questionsSection.container.classList.toggle("hide");
-        questionsSection.title.textContent = "My First Question";
-    };
+        parent.appendChild(choice);
+    }
+}
 
+//event handler for answering the question
+const onClickChoice = (e) => {
+    const index = parseInt(e.currentTarget.dataset.index);
+
+    if(index === correctAnswer)
+    {
+        //true
+        alert("yes");
+    }else{
+        //guessed wrong
+        alert("no!");
+    }
+
+    displayNextQuestion();
+}
+
+//get the next question and display it.
+const displayNextQuestion = () => {
+    const nextQuestion = qToPlay.length > 0 ? qToPlay.pop() : null;
+    if (nextQuestion === null) {
+        //that's it, game's over, show the results
+        return;
+    }
+
+    correctAnswer = nextQuestion.correct;
+    resetQuesionDisplay(questionsSection);
+    questionsSection.title.textContent = nextQuestion.title;
+    createChoiceChildren(questionsSection.choices, nextQuestion.answers);
+};
+
+//event handler for starting the game.
+const onClickStartButton = (e) => {
+    //we don't need the event hanlder anymore, so let's remove it.
+    e.currentTarget.removeEventListener("click", onClickStartButton);
+
+    //hide the start screen, so we can show the questions instead.
+    document.getElementById("start-screen").classList.toggle("hide");
+
+    //show the section with the questions
+    questionsSection.container.classList.toggle("hide");
+
+    displayNextQuestion();
+};
+
+const init = () => {
     document.querySelector("#start").addEventListener("click", onClickStartButton);
 };
 
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,50 +1,50 @@
 let questions = [
     {
         title: 'Inside which HTML element do we put the JavaScript?',
-        a1: "<javascript>",
-        a2: "<js>",
-        a3: "<scripting>",
-        a4: "<script>",
+        answers: ["<javascript>",
+            "<js>",
+            "<scripting>",
+            "<script>"],
         correct: 3 //index based
     },
     {
         title: 'Where is the correct place to insert a JavaScript?',
-        a1: "Both the <head> section and the <body> section are correct",
-        a2: "The <head> section.",
-        a3: "The <body> section.",
-        a4: "Nowhere, you should not use JavaScript.",
+        answers: ["Both the <head> section and the <body> section are correct",
+            "The <head> section.",
+            "The <body> section.",
+            "Nowhere, you should not use JavaScript."],
         correct: 0//index based
     },
     {
         title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-        a1: '<script name="xxx.js">',
-        a2: '<script src="xxx.js">',
-        a3: '<script href="xxx.js">',
-        a4: '<script file="xxx.js">',
+        answers: ['<script name="xxx.js">',
+            '<script src="xxx.js">',
+            '<script href="xxx.js">',
+            '<script file="xxx.js">'],
         correct: 1//index based
     },
     {
         title: 'How do you write "Hello World" in an alert box?"?',
-        a1: 'msg("Hello World");',
-        a2: 'alert("Hello World");',
-        a3: 'alertBox("Hello World");',
-        a4: 'msgBox("Hello World");',
+        answers: ['msg("Hello World");',
+            'alert("Hello World");',
+            'alertBox("Hello World");',
+            'msgBox("Hello World");'],
         correct: 1//index based
     },
     {
         title: 'How to write an IF statement in JavaScript?"?',
-        a1: 'if i = 5 then',
-        a2: 'if i == 5 then',
-        a3: 'if (i == 5)',
-        a4: 'if i = 5',
+        answers: ['if i = 5 then',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i = 5'],
         correct: 2//index based
     },
     {
         title: 'How does a FOR loop start?"?',
-        a1: 'for i = 1 to 5',
-        a2: 'for (i = 0; i <= 5)',
-        a3: 'for (i <= 5; i++)',
-        a4: 'for (i = 0; i <= 5; i++)',
+        answers: ['for i = 1 to 5',
+            'for (i = 0; i <= 5)',
+            'for (i <= 5; i++)',
+            'for (i = 0; i <= 5; i++)'],
         correct: 3//index based
     }
 ];
